3f301c1a62f10a7ce9272b04b21fc66ed5790259
lu <haofei.luu@gmail.com>
2024-07-08 14:34:46 +0200

    add exp3 options and resets

--- moveit_Unity.py
+++ moveit_Unity.py
@@ -707,34 +707,46 @@ def initialize():
                 print("Type in the current randomized condition, follow by the current turn, e.g., \"A2\", \"a2\", \"C4\",\"c4\".")
                 action = input()
                 # One block on table, pick up block 1 and place it
-                if action in {"A2", "a2", "B2", "b2", "E2", "e2"}:
+                if action in {"A2", "a2", "B2", "b2", "E2", "e2", "J2", "j2"}:
                     request = generate_request("1", "2")
                     tutorial.pickup_plan_cartesian(request)
+                    tutorial.reset_robot(0)
                 # One block on table, pick up block 1 but drop it
-                elif action in {"C2", "c2", "F2", "f2"}:   
+                elif action in {"C2", "c2", "F2", "f2", "K2", "k2"}:   
                     request = generate_picknoplace("1")
                     tutorial.pickup_plan_cartesian(request, drop = True)
+                    tutorial.reset_robot(0)
                 # one block on table, pickup block 1, knockover  stack, and drop the block you're holding
                 elif action in {"D2", "d2", "G2", "g2", "H2", "h2"}:
                     request = generate_knockover_request("1")
                     tutorial.knock_down_plan(request, copy.deepcopy(request), "drop")
+                    tutorial.reset_robot(0)
 
                 # three blocks on table, pick up block 2 and place it
                 elif action in {"A4", "a4"}:
                     request = generate_request("2", "4")
                     tutorial.pickup_plan_cartesian(request)
+                    tutorial.reset_robot(0)
+                # two blocks on table, pick up block 2 and place it
+                elif action in {"k4", "K4"}:
+                    request = generate_request("2", "3")
+                    tutorial.pickup_plan_cartesian(request)
+                    tutorial.reset_robot(0)
                 # One block on table, pick up block 2 but drop it
-                elif action in {"G4", "g4"}:   
+                elif action in {"G4", "g4", "J4", "j4"}:   
                     request = generate_picknoplace("2")
                     tutorial.pickup_plan_cartesian(request, drop = True)
+                    tutorial.reset_robot(0)
                 # some blocks on table, pickup block 2, knockover  stack, and drop the block you're holding
                 elif action in {"B4", "b4", "C4", "c4", "D4", "d4"}:
                     request = generate_knockover_request("2")
                     tutorial.knock_down_plan(request, copy.deepcopy(request), "drop")
+                    tutorial.reset_robot(0)
                 # some blocks on table, pickup block 2, knockover  stack, and stack the block you're holding
                 elif action in {"E4", "e4", "F4", "f4", "H4", "h4"}:
                     request = generate_knockover_request("2")
                     tutorial.knock_down_plan(request, copy.deepcopy(request), "place")
+                    tutorial.reset_robot(0)
                 else:
                     print("You entered an invalid condition/turn. Conditions are A, B, C, D, E, F, G and H. Turns are 1 and 2. Please try again.")
 
